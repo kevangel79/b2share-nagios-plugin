7cc653ad42b72a0ddd0b5bbad481d508f5e2fea9
a.memon <ahmed.shiraz.memon@gmail.com>
2016-10-05 14:09:47 +0200

    partially added return codes

--- check_b2access.py
+++ check_b2access.py
@@ -34,14 +34,17 @@ def getAccessToken(param):
         token = oauth.fetch_token(token_url=str(param.url)+TOKEN_URI, verify=False,client_id=str(param.username),client_secret=str(param.password),scope=['USER_PROFILE','GENERATE_USER_CERTIFICATE'])
         j = json.dumps(token, indent=4)
         k = json.loads(j)
-        print "Access token: "+k['access_token']
+        if param.verbose:
+            print "Access token: "+k['access_token']
         
         getTokenInfo(str(param.url)+'/oauth2/tokeninfo', str(k['access_token']), param.verbose)
         getUserInfo(str(param.url)+'/oauth2/userinfo', str(k['access_token']), param.verbose)
     except:
         print("CRITICAL: Error fetching OAuth 2.0 access token:", sys.exc_info()[0])
         raise
+        return 2
         sys.exit(2)
+        
         
 def getTokenInfo(url, token, verbose):
     """ Fetch access token details """
@@ -52,32 +55,33 @@ def getTokenInfo(url, token, verbose):
         entity = requests.get(url,verify=False, headers = {'Authorization': 'Bearer '+token})
         j = entity.json()
         expire = datetime.datetime.fromtimestamp(int(j['exp'])).strftime('%Y-%m-%d %H:%M:%S')
-        print "Expires in: "+expire
-        
         if verbose:
+            print "Expires in: "+expire
             print 'Detailed token info: '+entity.text
     except:
         print("CRITICAL: Error retrieving access token information:", sys.exc_info()[0])
         raise
+        return 2
         sys.exit(2)
+        
 
 def getUserInfo(url, token, verbose):
     """ Fetch user information using access token """
     try:
-        print "\n"
-        if verbose:
+        if param.verbose:
+            print "\n"
             print "Fetching user information based on access token, endpoint URL: "+url
         entity = requests.get(url,verify=False, headers = {'Authorization': 'Bearer '+token})
         j = entity.json()
-        print "Subject: "+j['sub']
-        print "Persistent Id: "+j['unity:persistent']
-        
-        if verbose:
+        if param.verbose:
+            print "Subject: "+j['sub']
+            print "Persistent Id: "+j['unity:persistent']
             print 'Detailed user information: '+entity.text
         
     except:
         print("CRITICAL: Error retrieving user information:", sys.exc_info()[0])
         raise
+        return 2
         sys.exit(2)
 
         
@@ -85,31 +89,31 @@ def getInfoUsernamePassword(param):
     """ Query user information with username and password """
     
     url = param.url+"/rest-admin/v1/resolve/userName/"+str(param.username)
-    print "\n"
+    
     if param.verbose:
-        print "Query with username and password, endpoint URL: "+url
+        print "\nQuery with username and password, endpoint URL: "+url
     
     try: 
         entity = requests.get(str(url),verify=False,auth=(str(param.username), str(param.password)))
         if entity.status_code == 403:
             print "Error occurred while resolving the given user: "+str(param.username)
+            return 1
             sys.exit(1)
         j = entity.json()
-        print "Credential requirement: "+j['credentialInfo']['credentialRequirementId']
-        print "Entity Id: "+str(j['id'])
-        print "Username: "+j['identities'][0]['value']
         if param.verbose:
+            print "Credential requirement: "+j['credentialInfo']['credentialRequirementId']
+            print "Entity Id: "+str(j['id'])
+            print "Username: "+j['identities'][0]['value']
             print "Detailed user information: "+entity.text
         
     except:
         print("CRITICAL: Error retrieving user information with username/password:", sys.exc_info()[0])
         raise
+        return 2
         sys.exit(2)
         
 def getInfoCert(param):
     """ Query user information with X509 Certificate Authentication """
-    
-    
     try:
         cert_txt = subprocess.check_output(["openssl", "x509", "-subject", "-noout","-in", param.certificate])
     
@@ -121,25 +125,26 @@ def getInfoCert(param):
     
         url = param.url+"/rest-admin/v1/resolve/x500Name/"+dn
         
-        print "\n"
-        
         if param.verbose:
-            print "Query user information with X509 Certificate Authentication, endpoint URL:" + url
+            print "\nQuery user information with X509 Certificate Authentication, endpoint URL:" + url
         
         entity = requests.get(str(url),verify=False,cert=(str(param.certificate), str(param.key)))
         j = entity.json()
-        print "Credential requirement: "+j['credentialInfo']['credentialRequirementId']
-        print "Entity Id: "+str(j['id'])
-        print "X500Name: "+j['identities'][0]['value']
+        if param.verbose:
+            print "Credential requirement: "+j['credentialInfo']['credentialRequirementId']
+            print "Entity Id: "+str(j['id'])
+            print "X500Name: "+j['identities'][0]['value']
         
         if entity.status_code == 403:
             print "CRITICAL: Error occurred while resolving the given user: "+str(param.username)
-            exit(2)
+            return 2
+            sys.exit(2)
         if param.verbose:
             print "Detailed user information: "+entity.text
     except:
         print("CRITICAL: Error retrieving user information by X509 certificate:", sys.exc_info()[0])
         raise
+        return 2
         sys.exit(2)
 
 def getLdapName(openssl_name):
@@ -197,16 +202,16 @@ if __name__ == '__main__':
         print "Timeout: "+timeout
         signal.signal(signal.SIGALRM, handler)
         signal.alarm(int(param.timeout))
-        
-    print "Starting B2ACCESS Probe...\n---------------------------\n"
-    print "B2ACCESS url: "+str(base_url)
-    print "B2ACCESS username: "+username
-    print "Public key: "+param.certificate   
+    if param.verbose:    
+        print "Starting B2ACCESS Probe...\n---------------------------\n"
+        print "B2ACCESS url: "+str(base_url)
+        print "B2ACCESS username: "+username
+        print "Public key: "+param.certificate   
     getAccessToken(param)
     getInfoUsernamePassword(param)
     getInfoCert(param)
     if param.verbose:
-        print "\nOK, User access token retrieval and login via X.509 Certificate and username/password were successful"
+        print "OK" 
     else:
-        print "OK"
+        print "\nOK, User access token retrieval and login via X.509 Certificate and username/password were successful" 
     sys.exit(0)
